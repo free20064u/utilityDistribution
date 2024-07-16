4a4fa33825786fc24e2f8201750c18d5952587b6
Alfred <76535763+free20064u@users.noreply.github.com>
2024-07-16 01:55:38 -0700

    bill interface added

--- utils/urls.py
+++ utils/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.homepage, name='home'),
     path('dashboard/', views.dashboardView, name='dashboard'),
     path('utilityBills/', views.utilityBillsView, name='utilityBills'),
+    path('bills/', views.billsView, name='bills'),
+    path('editBill/<int:id>', views.editBillView, name='editBill'),
 ]
--- utils/views.py
+++ utils/views.py
@@ -4,8 +4,39 @@ from django.contrib import messages
 
 from accounts.forms import UserLoginForm
 from .forms import DustbinForm
+from .models import UtilBill
 
 # Create your views here.
+def editBillView(request, id=None):
+    oldBill = UtilBill.objects.get(id=id)
+    form = DustbinForm(instance=oldBill, initial={'dustbinBill':oldBill.dustbinBill, 'electricBill':oldBill.electricBill, 'waterBill':oldBill.waterBill, 'user':request.user})
+    context = {
+        'form': form,
+    }
+    if request.method == 'POST':
+        form = DustbinForm(request.POST, instance=UtilBill.objects.get(id=id))
+        print(form)
+        if form.is_valid():
+            bill = form.save(commit=False)
+            bill.user = request.user
+            bill.save()
+            messages.success(request, 'This month bill edited.')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Bill not edited.')
+            return render(request, 'utils/dustbin.html', context)
+    else:
+        return render(request, 'utils/dustbin.html', context)
+
+
+def billsView(request):
+    bills = UtilBill.objects.filter(user_id=request.user.id).order_by('-dateAdded')
+    context= {
+        'bills': bills,
+    }
+    return render(request, 'utils/bills.html', context)
+
+
 def utilityBillsView(request):
     form = DustbinForm(initial={'dustbinBill':0.00, 'electricBill':0.00, 'waterBill':0.00, 'user':request.user})
     context = {
