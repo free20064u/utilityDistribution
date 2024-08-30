94bf1a3139d6c64975fcfcd7e7bbf99476d52f7c
Alfred <76535763+free20064u@users.noreply.github.com>
2024-08-29 17:00:11 -0700

    undo deploy

--- utils/urls.py
+++ utils/urls.py
@@ -23,5 +23,4 @@ urlpatterns = [
     path('payBill/<int:id>/', views.payBillView, name='payBill'),
     path('addPeople/<int:id>/', views.addPeopleView, name='addPeople'),
     path('editNoOfPeople/<int:id>/', views.editNoOfPeopleView, name='editNoOfPeople'),
-    path('git_update/', views.gitUpdateView, name='git_update'),
  ]
