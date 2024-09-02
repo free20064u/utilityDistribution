87dce377952c86c328daee2822f6fda10df59c5e
Alfred <76535763+free20064u@users.noreply.github.com>
2024-09-01 17:44:57 -0700

    allowd host edited again

--- core/settings.py
+++ core/settings.py
@@ -25,10 +25,12 @@ SECRET_KEY = 'django-insecure-8cg14ira^(tude$n2un7d#&0qq1*gvwdr^b%f0#myst#s-$_es
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+ALLOWED_HOSTS = ''
+
 if DEBUG:
     ALLOWED_HOSTS = ['192.168.43.180', '127.0.0.1']
 else:
-    ALLOWED_HOSTS = ['https://utilitydistribution.pythonanywhere.com/']
+    ALLOWED_HOSTS = ['utilitydistribution.pythonanywhere.com']
 
 
 # Application definition
